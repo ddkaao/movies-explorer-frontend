a03a4a8259f70bf5d79eb7f209543c1ca2ddb2e2
Dmitry Kirshin <dmitry_wattt@mail.ru>
2024-04-13 17:16:11 +0300

    refactor: отзывчивая верстка

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
             <div className="footer__container">
                 <h2 className="footer__caption">Учебный проект Яндекс.Практикум х BeatFilm.</h2>
                 <div className="footer__content">
-                    <p className="footer__copyright">© 2020</p>
+                    <p className="footer__copyright">© 2024</p>
                     <div className="footer__links">
                         <a href="https://practicum.yandex.ru/" className="footer__link" rel="noreferrer" target="_blank">Яндекс.Практикум</a>
                         <a href="https://github.com/ddkaao" className="footer__link" rel="noreferrer" target="_blank">GitHub</a>
